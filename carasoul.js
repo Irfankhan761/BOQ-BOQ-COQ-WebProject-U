6b12daaf7906c2a2d8806e9c0f544cca33f4c460
Irfankhan761 <irfandk1994@gmail.com>
2024-01-05 21:40:14 +0500

    carasoul added

---
+++ carasoul.js
@@ -0,0 +1,166 @@
+
+const carouselData = [
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cd3905e129e4.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cd3905e11943.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c40c0840e54f.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c40c0840d5f0.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c89ae0fe87f5.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c40096210546.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c068078ccfb9.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5ce7f5360446f.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c2e2364747fb.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5be47ff15177a.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cdc46149de9c.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cc351113b311.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cc0c0d80676d.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cc23af24a05e.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5d128a3570e58.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5d128a3493363.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5c685730a5080.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5d0a6ff679c0f.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5d77ec0f31c55.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5db9c81ac794d.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+    {
+      backgroundImage: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5bfd9fe647c64.png",
+      imageSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/5cc23a5bbd14e.png",
+      shopLink: "#",
+      arrowSrc: "https://rawcdn.githack.com/SochavaAG/example-mycode/master/pens/parallax-swipe/images/arrow.svg",
+      title: "Boq Boq Coq",
+      products: "Check it out",
+    },
+  
+  ];
+  
+
+
+function addDataToFlickitySlider() {
+    const flickitySlider = $('.js-flickity-slider');
+
+
+    carouselData.forEach(item => {
+        const carouselCell = $('<div class="js-carousel-cell"></div>');
+        carouselCell.html(`
+            <!-- Your HTML structure for each carousel cell -->
+            <div class="ag-shop-card_box-wrap">
+                <div class="ag-shop-card_box">
+                    <div class="ag-shop-card_body">
+                        <div class="js-card-bg ag-card-bg" style="background-image: url(${item.backgroundImage});"></div>
+                        <a href="${item.shopLink}" class="ag-shop-card-body_link"><img src="${item.imageSrc}" alt="${item.title}" class="ag-shop-card-body_img" /></a>
+                    </div>
+                    <div class="ag-shop-card_footer">
+                        <a href="${item.shopLink}" class="ag-shop-card-footer_link"><img src="${item.arrowSrc}" class="ag-shop-card-footer_arrow" /></a>
+                        <span class="ag-shop-card-footer_title">${item.title}</span>
+                        <span class="ag-shop-card-footer_products">${item.products}</span>
+                    </div>
+                </div>
+            </div>
+        `);
+        flickitySlider.append(carouselCell);
+    });
+}
+
+
+addDataToFlickitySlider();
+
+(function ($) {
+    $(function () {
+      var agSlideFlickity = $(".js-flickity-slider").flickity({
+    autoPlay: 2000,
+      imagesLoaded: true,
+      percentPosition: false,
+      prevNextButtons: false,
+      initialIndex: 5,
+      pageDots: false,
+      groupCells: 1,
+      });
+  
+      var agCard = agSlideFlickity.find(".js-carousel-cell .js-card-bg"),
+        agTransform =
+          "string" == typeof document.documentElement.style.transform
+            ? "transform"
+            : "WebkitTransform",
+        agSlide = agSlideFlickity.data("flickity");
+  
+      agSlideFlickity.on("scroll.flickity", function () {
+        agSlide.slides.forEach(function (t, e) {
+          var n = agCard[e],
+            i = (-1 * (t.target + agSlide.x)) / 3;
+  
+          n.style[agTransform] = "translateX(" + i + "px)";
+        });
+      });
+  
+      agSlideFlickity.on("dragStart.flickity", function (t, e) {
+        document.ontouchmove = function (t) {
+          t.preventDefault();
+        };
+      });
+  
+      agSlideFlickity.on("dragEnd.flickity", function (t, e) {
+        document.ontouchmove = function (t) {
+          return true;
+        };
+      });
+    });
+  })(jQuery);
+  
